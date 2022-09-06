56f2e12485dcf1d8a83ac8e1dda3bc746520ad40
amitav.singh <singh.amitav26@gmail.com>
2022-09-06 14:43:18 +0200

    Package Structure for Clean Architecture

---
+++ app/src/main/java/com/amitav/cleanarchitecture/notesapp/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.amitav.cleanarchitecture.notesapp.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.amitav.cleanarchitecture.notesapp.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
